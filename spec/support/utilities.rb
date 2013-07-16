1d97068b91684064b3c13cd90b370c19fccd260a
Ben Gelsey <ben.gelsey@gmail.com>
2013-07-16 13:44:40 -0700

    Finish signin/signout

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,18 @@
 include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
